6dacf202a8d6da140216eb261becae3aa293d201
Abdulazeez <hammedabdulazeez05@gmail.com>
2025-08-13 18:38:38 +0100

    Latest

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import RevealOnScroll from '../RevealOnScroll'
+import ecommerce from '../../assets/forever.PNG'
+import cooking from '../../assets/cooking.png'
+import leaderboard from '../../assets/leaderboard.png'
+import photofolio from '../../assets/Photofolio.png'
+import RevealOnScroll from '../RevealOnScroll';
 
 const Project = () => {
   return (
@@ -10,50 +14,55 @@ const Project = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
           <div className=" p-6 rounded-xl border-white/10 border hover:-translate-y-1  hover:border-blue-500/30  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
-            <h3 className="text-xl font-semibold mb-4">Project Title 1</h3>
-            <p className="text-gray-400 mb-4">Short description of the project.</p>
+          <img src={ecommerce} alt="" />
+            <h3 className="text-xl font-semibold mb-4">Forever E-commerce Website</h3>
+            {/* <p className="text-gray-400 mb-4">Short description of the project.</p> */}
             <div>
-                {["HTML", "CSS", "JavaScript", "React"].map((skill, index) => (
+                {["HTML", "CSS", "JavaScript", "React","Tailwind css"].map((skill, index) => (
                   <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{skill}</span>
                 ))}
             </div>
             <div className='flex justify-between items-center mt-4 text-center'>
-                <a href="#" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
+                <a href="https://github.com/Azeez-05/forever-ecommerce" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
             </div>
           </div>
         
           <div className=" p-6 rounded-xl border-white/10 border hover:-translate-y-1  hover:border-blue-500/30  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
-            <h3 className="text-xl font-semibold mb-4">Project Title 2</h3>
-            <p className="text-gray-400 mb-4">Short description of the project.</p>
+              <img src={cooking} alt="" />
+            <h3 className="text-xl font-semibold mb-4">An Recipe Website</h3>
+            {/* <p className="text-gray-400 mb-4">Short description of the project.</p> */}
             <div>
-                {["HTML", "CSS", "JavaScript", "React"].map((skill, index) => (
+                {["HTML", "CSS", "JavaScript", "Bootstrap"].map((skill, index) => (
                   <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{skill}</span>
                 ))}
             </div>
             <div className='flex justify-between items-center mt-4 text-center'>
-                <a href="#" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
+                <a href="https://github.com/Azeez-05/food-recipe" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
             </div>
           </div>
         
           <div className=" p-6 rounded-xl border-white/10 border hover:-translate-y-1  hover:border-blue-500/30  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
-            <h3 className="text-xl font-semibold mb-4">Project Title 3</h3>
-            <p className="text-gray-400 mb-4">Short description of the project.</p>
+              <img src={photofolio} alt="" />
+            <h3 className="text-xl font-semibold mb-4">PhotoFolio</h3>
+            {/* <p className="text-gray-400 mb-4">Short description of the project.</p> */}
             <div>
-                {["HTML", "CSS", "JavaScript", "React"].map((skill, index) => (
+                {["HTML", "CSS", "JavaScript", "Bootstrap"].map((skill, index) => (
                   <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{skill}</span>
                 ))}
             </div>
             <div className='flex justify-between items-center mt-4 text-center'>
-                <a href="#" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
+                <a href="https://github.com/Azeez-05/photofolio" className="text-blue-500 hover:text-blue-300 transition-colors">View Project</a>
             </div>
           </div>
 
           <div className=" p-6 rounded-xl border-white/10 border hover:-translate-y-1  hover:border-blue-500/30  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
-            <h3 className="text-xl font-semibold mb-4">Project Title 4</h3>
-            <p className="text-gray-400 mb-4">Short description of the project.</p>
+              <img src={leaderboard} alt="" />
+
+            <h3 className="text-xl font-semibold mb-4">Participant Leaderboard</h3>
+            {/* <p className="text-gray-400 mb-4">Short description of the project.</p> */}
             <div>
-                {["HTML", "CSS", "JavaScript", "React"].map((skill, index) => (
-                  <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{skill}</span>
+                {["HTML", "CSS", "JavaScript", "Boostrap"].map((skill, index) => (
+                  <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{skill} </span>
                 ))}
             </div>
             <div className='flex justify-between items-center mt-4 text-center'>
--- src/components/sections/SocialLinks.jsx
+++ src/components/sections/SocialLinks.jsx
@@ -8,15 +8,21 @@ const SocialLinks = () => {
       <div className="w-170  mx-auto px-4">
         <h2 className="text-5xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent">Get In Touch</h2>
         <h2 className='text-center font-bold mt-4'>Got a project we can work on together?</h2>
-        <div className='my-10 grid grid-cols-3 md:grid-cols-3 gap-6 pl-10'>
+        <div className='my-10 grid grid-cols-5 md:grid-cols-5 gap-6 pl-10'>
           <div>
-             <a className="bi bi-linkedin text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://github.com/Azeez-05'></a>
+             <a className="bi bi-linkedin text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://www.linkedin.com/in/abdulazeez-hammed'></a>
           </div>
           <div> 
             <a className="bi bi-envelope-at text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://mailto:hammedabdulazeez05@gmail.com'></a>
           </div>
           <div> 
-            <a className="bi bi-github text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://www.linkedin.com/in/abdulazeez-hammed'></a>
+            <a className="bi bi-github text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://github.com/Azeez-05'></a>
+          </div>
+          <div> 
+            <a className="bi bi-tiktok text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://tiktok.com/@dev___zyz'></a>
+          </div>
+          <div> 
+            <a className="bi bi-twitter-x text-6xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-violet-600 bg-clip-text text-center text-transparent" href='https://x.com/Dev_zyz'></a>
           </div>
         </div>
          <h2 className='text-center font-extralight mt-5 '>Let's Create Something Amazing</h2>
